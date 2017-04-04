3fa4c4c92eaf414f42f800f4dd1d652df8e186d3
Philip Walton <philip@philipwalton.com>
2017-04-03 21:22:13 -0700

    Fix incorrect indentation

--- src/analytics/multiple-trackers.js
+++ src/analytics/multiple-trackers.js
@@ -264,40 +264,40 @@ const sendInitialPageview = () => {
  * Gets the DOM and window load times and sends them as custom metrics to
  * Google Analytics via an event hit.
  */
- const sendNavigationTimingMetrics = () => {
-   // Only track performance in supporting browsers.
-   if (!(window.performance && window.performance.timing)) return;
+const sendNavigationTimingMetrics = () => {
+  // Only track performance in supporting browsers.
+  if (!(window.performance && window.performance.timing)) return;
 
-   // If the window hasn't loaded, run this function after the `load` event.
-   if (document.readyState != 'complete') {
-     window.addEventListener('load', sendNavigationTimingMetrics);
-     return;
-   }
+  // If the window hasn't loaded, run this function after the `load` event.
+  if (document.readyState != 'complete') {
+    window.addEventListener('load', sendNavigationTimingMetrics);
+    return;
+  }
 
-   const nt = performance.timing;
-   const navStart = nt.navigationStart;
+  const nt = performance.timing;
+  const navStart = nt.navigationStart;
 
-   const responseEnd = Math.round(nt.responseEnd - navStart);
-   const domLoaded = Math.round(nt.domContentLoadedEventStart - navStart);
-   const windowLoaded = Math.round(nt.loadEventStart - navStart);
+  const responseEnd = Math.round(nt.responseEnd - navStart);
+  const domLoaded = Math.round(nt.domContentLoadedEventStart - navStart);
+  const windowLoaded = Math.round(nt.loadEventStart - navStart);
 
-   // In some edge cases browsers return very obviously incorrect NT values,
-   // e.g. 0, negative, or future times. This validates values before sending.
-   const allValuesAreValid = (...values) => {
-     return values.every((value) => value > 0 && value < 6e6);
-   };
+  // In some edge cases browsers return very obviously incorrect NT values,
+  // e.g. 0, negative, or future times. This validates values before sending.
+  const allValuesAreValid = (...values) => {
+    return values.every((value) => value > 0 && value < 6e6);
+  };
 
-   if (allValuesAreValid(responseEnd, domLoaded, windowLoaded)) {
-     gaTest('send', 'event', {
-       eventCategory: 'Navigation Timing',
-       eventAction: 'track',
-       nonInteraction: true,
-       [metrics.RESPONSE_END_TIME]: responseEnd,
-       [metrics.DOM_LOAD_TIME]: domLoaded,
-       [metrics.WINDOW_LOAD_TIME]: windowLoaded,
-     });
-   }
- };
+  if (allValuesAreValid(responseEnd, domLoaded, windowLoaded)) {
+    gaTest('send', 'event', {
+      eventCategory: 'Navigation Timing',
+      eventAction: 'track',
+      nonInteraction: true,
+      [metrics.RESPONSE_END_TIME]: responseEnd,
+      [metrics.DOM_LOAD_TIME]: domLoaded,
+      [metrics.WINDOW_LOAD_TIME]: windowLoaded,
+    });
+  }
+};
 
 
 /**
